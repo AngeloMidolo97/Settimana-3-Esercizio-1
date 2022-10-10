1fc4a199b7d905a96a16091d702f851de8943419
Angelo Midolo <113070666+AngeloMidolo97@users.noreply.github.com>
2022-10-10 15:24:05 +0200

    commit

---
+++ ESERCIZIO/assets/js/naming.js
@@ -0,0 +1,25 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+document.getElementById('pet').innerHTML = petPreferito;
+
+var PetPreferito = 'coniglio';
+document.getElementById('coniglio').innerHTML = PetPreferito;
+
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
+
+var primoNumero = 5;
+var secondoNumero = 6;
+
+console.log(primoNumero + secondoNumero); //Somma tra due numeri
